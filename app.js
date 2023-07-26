4f1ca049f4670126095dfdf8273808961892be4d
kkauma <kylekauma@gmail.com>
2023-07-25 20:51:36 -0600

    test server on localhost:3000

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
+// const express = require("express");
+// const path = require("path");
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//   console.log("Database connected");
+// });
+
+// const app = express();
+
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Serving on port 3000");
+// });
+
+// Testing below:
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
-
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 app.get("/", (req, res) => {
-  res.render("home");
+  res.send("Hello from YelpCamp!");
 });
 
 app.listen(3000, () => {
