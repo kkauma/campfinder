92131ca6250099a1703a7be53d480bc81504e77d
kkauma <kylekauma@gmail.com>
2023-07-25 20:36:20 -0600

    update the models for a campground

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
