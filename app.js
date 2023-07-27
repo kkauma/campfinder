b6a77f82c578143c228f9a77ca40a3502ff1c546
kkauma <kylekauma@gmail.com>
2023-07-26 18:00:56 -0600

    connect mongoose to mongodb database, reconfigure local host

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//   console.log("Database connected");
-// });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
