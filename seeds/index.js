a318b2d62a60a50546a4380a8b203c4a059a40bb
kkauma <kylekauma@gmail.com>
2023-07-29 17:45:37 -0600

    create 50 campgrounds for testing, make sure all are saved to the database

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const cities = require("./cities");
+const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
@@ -11,6 +12,13 @@ db.once("open", () => {
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  const c = new Campground({ title: "Rocky Mountains" });
-  await c.save();
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
 };
+
+seedDB();
