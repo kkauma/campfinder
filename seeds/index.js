6548bb0d29885aab4203fb11a0e70bb3664089bb
kkauma <kylekauma@gmail.com>
2023-07-29 17:52:48 -0600

    add titles to each campground by utilizing the seedHelpers file

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -10,15 +11,20 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
     await camp.save();
   }
 };
 
-seedDB();
+seedDB().then(() => {
+  mongoose.connection.close();
+});
