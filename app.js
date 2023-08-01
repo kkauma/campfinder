cfa7704561d45a1a143e6db29e3b4bba00de09a2
kkauma <kylekauma@gmail.com>
2023-07-31 21:35:42 -0600

    finish edit campground route

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
 });
 
 app.put("/campgrounds/:id", async (req, res) => {
-  res.send("IT WORKED!");
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
