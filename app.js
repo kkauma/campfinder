8a9a551f0995b7f652e2ceb37d0ea9bd4c75009b
kkauma <kylekauma@gmail.com>
2024-09-15 18:07:15 -0700

    install ejs-mate and create boilerplate file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -13,6 +14,7 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -32,7 +34,6 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-// Test route
 app.get("/campgrounds/create", async (req, res) => {
   res.render("campgrounds/create");
 });
