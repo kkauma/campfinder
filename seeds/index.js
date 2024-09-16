b9ab546001b5aa9cfe0017de9f7e70f7f1b035fa
kkauma <kylekauma@gmail.com>
2024-09-15 18:43:36 -0700

    add images and descriptions to each campground, seed database with new data

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quas quibusdam blanditiis officiis odio numquam repudiandae architecto quo provident cupiditate? Dicta, neque perferendis. Voluptatem accusamus quidem commodi a quam esse?",
+      price,
     });
     await camp.save();
   }
