34978d3dec939709511a87c49f74331604e331bb
kkauma <kylekauma@gmail.com>
2024-11-03 08:47:23 -0700

    define the review model

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -7,6 +7,12 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
