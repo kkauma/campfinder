085ba2538f1f1adcf9be8c8b48ea1bd61d275749
kkauma <kylekauma@gmail.com>
2024-11-18 15:42:13 -0700

    Set up serving static assets

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
