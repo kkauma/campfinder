542da89cbbdde351227239c592aba13cfbf403c9
kkauma <kylekauma@gmail.com>
2024-11-18 19:19:24 -0700

    Add flash error to edit route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -55,6 +55,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "Can't find that campground!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
