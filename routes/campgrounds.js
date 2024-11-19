7dc1608eb377320e9af1c7a942137582a61e167f
kkauma <kylekauma@gmail.com>
2024-11-18 17:24:44 -0700

    Install flash and set up success middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,6 +33,7 @@ router.post(
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash("success", "Successfully made a new campground!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
