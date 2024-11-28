a08b1b7e6798a10ec1c6ba925e0ada2f02666569
kkauma <kylekauma@gmail.com>
2024-11-28 15:33:32 -0700

    Add routes for logging in

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
@@ -21,6 +22,22 @@ router.post(
       res.redirect("register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/campgrounds");
+  }
 );
 
 module.exports = router;
