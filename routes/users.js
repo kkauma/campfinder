187f1a26301c8de5dc763093ffc2bf0d2a143c34
kkauma <kylekauma@gmail.com>
2024-12-01 16:14:06 -0700

    Implement return to behavior

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
+const { storeReturnTo } = require("../middleware");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -33,13 +34,16 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  storeReturnTo,
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
   }),
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = res.locals.returnTo || "/campgrounds";
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
 
