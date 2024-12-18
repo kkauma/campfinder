296bdcdeb5adff2b193a673cf790233d20bdf87c
kkauma <kylekauma@gmail.com>
2024-12-18 10:27:28 -0700

    Add camground permissions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -75,7 +75,12 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You don't have permission to do that!");
+      return res.redirect(`/campgrounds/${id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
     req.flash("success", "Successfully updated campground!");
