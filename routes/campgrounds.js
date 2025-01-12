dfefdd7ac8a1e0bec7674fd5168990df7f57fd17
kkauma <kylekauma@gmail.com>
2025-01-11 20:24:46 -0700

    Add functionality to add multiple files

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,8 +19,9 @@ router
   //   validateCampground,
   //   catchAsync(campgrounds.createCampground)
   // );
-  .post(upload.single("image"), (req, res) => {
-    res.send(req.body);
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("It worked!");
   });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
