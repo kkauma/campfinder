b94314a543f5672414c8afbc5c188c1278e53eeb
kkauma <kylekauma@gmail.com>
2025-01-20 20:09:38 -0700

    Re-seed the database

--- seeds/index.js
+++ seeds/index.js
@@ -19,12 +19,17 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      // Your User ID
       author: "6748ec3398332f3633f11afe",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quas quibusdam blanditiis officiis odio numquam repudiandae architecto quo provident cupiditate? Dicta, neque perferendis. Voluptatem accusamus quidem commodi a quam esse?",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/dmjxujfwz/image/upload/v1737233574/YelpCamp/boayipxhky32kviqqpto.jpg",
