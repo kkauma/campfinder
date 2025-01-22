51591062357ff2413432c65452afa16c30b10edd
kkauma <kylekauma@gmail.com>
2025-01-21 20:26:36 -0700

    Add controls to mapbox

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,6 +7,8 @@ const map = new mapboxgl.Map({
   zoom: 9, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
