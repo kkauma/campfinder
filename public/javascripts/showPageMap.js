830ba46d0d18ccfa96b70e5d1c7f520d560f17dd
kkauma <kylekauma@gmail.com>
2025-01-22 08:04:59 -0700

    Clean up navbar styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
-  // style: "mapbox://styles/mapbox/light-v10", // style URL
+  style: "mapbox://styles/mapbox/light-v10", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
