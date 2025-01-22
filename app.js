d11d613b004747f600bad301ec294e0970c98e7f
kkauma <kylekauma@gmail.com>
2025-01-22 14:52:38 -0700

    Remove unnecessary images

--- app.js
+++ app.js
@@ -20,12 +20,10 @@ const campgroundRoutes = require("./routes/campgrounds.js");
 const reviewRoutes = require("./routes/reviews.js");
 
 const MongoStore = require("connect-mongo");
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
-// "mongodb://127.0.0.1:27017/yelp-camp"
+const dbUrl = process.env.DB_URL;
 
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
